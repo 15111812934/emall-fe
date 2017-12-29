d5b710f24f48524d304348462da8e7ea45bd01e1
yangmei <3165690374@qq.com>
2017-12-29 17:14:01 +0800

    用户功能模块

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -2,13 +2,9 @@
 * @Author: yangmei
 * @Date:   2017-12-26 16:47:13
 * @Last Modified by:   yangmei
-* @Last Modified time: 2017-12-28 21:46:16
+* @Last Modified time: 2017-12-29 16:48:39
 */
 require('./index.css');
 require('page/common/nav/index.js');
 require('page/common/header/index.js');
-// var navSide=require('page/common/nav-side/index.js');
 
-// navSide.init({
-// 	name:'user-center'
-// });
